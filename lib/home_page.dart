f2b0cef2939cfd1d89fef69deeb48a4fe7e37041
Arya Vijayan <aryavijayanrohini@gmail.com>
2024-01-13 22:35:35 +0530

    speechtotext added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:mavis/feature_box.dart';
 import 'package:mavis/pallete.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,41 +12,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Mavis'),
-          leading: const Icon(Icons.menu),
-          centerTitle: true,
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: const Text('Mavis'),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-
             //profile
             Stack(
               children: [
                 Center(
                   child: Container(
-                      height: 150,
-                      width: 130,
-                      margin: const EdgeInsets.only(top: 4),
-                      decoration: const BoxDecoration(
-                            color: Pallete.assistantCircleColor,
-                            shape:BoxShape.circle,
-                          ),
-                      ),
+                    height: 150,
+                    width: 130,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
                 ),
                 Container(
                   height: 145,
                   decoration: const BoxDecoration(
                     shape: BoxShape.circle,
-                    image: DecorationImage(image: AssetImage(
-                      'assets/images/assist1.png'
-                      ),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/assist1.png'),
                     ),
                   ),
-
                 ),
               ],
             ),
@@ -55,7 +93,7 @@ class _HomePageState extends State<HomePage> {
                 horizontal: 20,
                 vertical: 10,
               ),
-              margin:const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
                 top: 10,
               ),
               decoration: BoxDecoration(
@@ -66,9 +104,9 @@ class _HomePageState extends State<HomePage> {
                   topLeft: Radius.zero,
                 ),
               ),
-             child : const Padding(
-               padding: EdgeInsets.symmetric(vertical: 10.0),
-               child: Text(
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
                   'Good Morning Arya,\nWhat can I do for you?',
                   style: TextStyle(
                     color: Pallete.assistantCircleColor,
@@ -76,7 +114,7 @@ class _HomePageState extends State<HomePage> {
                     fontFamily: 'Cera Pro',
                   ),
                 ),
-             ),
+              ),
             ),
             Container(
               padding: const EdgeInsets.all(10),
@@ -102,30 +140,42 @@ class _HomePageState extends State<HomePage> {
                 FeatureBox(
                   color: Pallete.firstSuggestionBoxColor,
                   HeaderText: 'ChatGPT',
-                  Description: 'A smarter way to stay organized and informed with ChatGPT',
-                  ),
-
-                  FeatureBox(
+                  Description:
+                      'A smarter way to stay organized and informed with ChatGPT',
+                ),
+                FeatureBox(
                   color: Pallete.secondSuggestionBoxColor,
                   HeaderText: 'Dall-E',
-                  Description: 'Get inspired and stay creative with your personal assistant powered by Dall-E',
-                  ),
-
-                  FeatureBox(
+                  Description:
+                      'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                ),
+                FeatureBox(
                   color: Pallete.thirdSuggestionBoxColor,
                   HeaderText: 'Smart Voice Assistant',
-                  Description: 'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
-                  ),
+                  Description:
+                      'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+                ),
               ],
             ),
-            
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Pallete.firstSuggestionBoxColor,
-          onPressed: () {},
-          child: const Icon(Icons.mic,color: Pallete.blackColor,),
-          ),
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+         }
+        },
+        child: const Icon(
+          Icons.mic,
+          color: Pallete.blackColor,
+        ),
+      ),
+    );
   }
 }
